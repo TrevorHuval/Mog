53ae98e9c8d8ba2cef43a8e66724568c71a94c25
Blake Lalonde <78009242+theblakelalonde@users.noreply.github.com>
2021-10-11 20:32:34 -0500

    Updated Profile and big update to Home

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:firstapp/group.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -18,79 +17,88 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    margin: EdgeInsets.fromLTRB(25, 25, 0, 0),
-                    child: Text("Current Group:",
-                        style: TextStyle(fontSize: 17),
-                        textAlign: TextAlign.left),
-                  ),
-                  Container(
-                      height: 250,
-                      width: 500,
-                      color: Colors.transparent,
-                      child: Card(
-                        color: Colors.grey.shade300,
-                        margin: const EdgeInsets.fromLTRB(20, 5, 20, 0),
-                        elevation: 7,
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: InkWell(
-                          splashColor: Colors.grey,
-                          onTap: () {},
-                          child: Column(
-                            children: <Widget>[
-                              const SizedBox(
-                                height:
-                                    15, // Creates margin inside the card for the group pic and name
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.fromLTRB(25, 25, 0, 0),
+                        child: Text("Current Group:",
+                            style: TextStyle(fontSize: 17),
+                            textAlign: TextAlign.left),
+                      ),
+                      Container(
+                          height: 250,
+                          width: 500,
+                          color: Colors.transparent,
+                          child: Card(
+                            color: Colors.grey.shade300,
+                            margin: const EdgeInsets.fromLTRB(20, 5, 20, 0),
+                            elevation: 7,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: InkWell(
+                              splashColor: Colors.grey,
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Group()));
+                              },
+                              child: Column(
+                                children: <Widget>[
+                                  const SizedBox(
+                                    height:
+                                        15, // Creates margin inside the card for the group pic and name
+                                  ),
+                                  Expanded(
+                                      child: Row(children: <Widget>[
+                                    Container(
+                                        width: 125,
+                                        height: 125,
+                                        margin:
+                                            EdgeInsets.fromLTRB(10, 0, 25, 0),
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                            image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: AssetImage(
+                                                    'assets/images/gooberGroupPFP.jpg')))),
+                                    Expanded(
+                                      child: Text("Goober",
+                                          style: TextStyle(fontSize: 40),
+                                          textAlign: TextAlign.start),
+                                    ),
+                                  ])),
+                                  Expanded(
+                                      child: ListView.separated(
+                                    padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                                    scrollDirection: Axis.horizontal,
+                                    itemCount: 6,
+                                    separatorBuilder: (context, _) =>
+                                        SizedBox(width: 12),
+                                    itemBuilder: (context, index) =>
+                                        buildProfilePics(),
+                                  )),
+                                ],
                               ),
-                              Expanded(
-                                  child: Row(children: <Widget>[
-                                Container(
-                                    width: 125,
-                                    height: 125,
-                                    margin: EdgeInsets.fromLTRB(10, 0, 25, 0),
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Colors.white,
-                                        image: DecorationImage(
-                                            fit: BoxFit.cover,
-                                            image: AssetImage(
-                                                'assets/images/gooberGroupPFP.jpg')))),
-                                Expanded(
-                                  child: Text("Goober",
-                                      style: TextStyle(fontSize: 40),
-                                      textAlign: TextAlign.start),
-                                ),
-                              ])),
-                              Expanded(
-                                  child: ListView.separated(
-                                padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
-                                scrollDirection: Axis.horizontal,
-                                itemCount: 6,
-                                separatorBuilder: (context, _) =>
-                                    SizedBox(width: 12),
-                                itemBuilder: (context, index) =>
-                                    buildProfilePics(),
-                              )),
-                            ],
-                          ),
-                        ),
-                      ))
-                ],
-              ),
-            ),
-          )
-        ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
