280ddb3ae153f5901cca7e8ce7a7ea16c56ed2dc
Anthony Duong <anthonyduong17@gmail.com>
2021-10-12 00:57:53 -0500

    Update nutrition.dart

--- lib/nutrition.dart
+++ lib/nutrition.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,15 +7,41 @@ class Nutrition extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-              child: Text(
-            "Nutrition Page",
-            style: TextStyle(fontSize: 20, fontFamily: 'SanFran'),
-          ))
-        ],
+      body: Container(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.fromLTRB(50, 25, 50, 0),
+                child: Text("Today",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center),
+              ),
+              Container(
+                width: 500,
+                height: 150,
+                color: Colors.transparent,
+                child: Card(
+                  color: Colors.grey.shade200,
+                  margin: EdgeInsets.fromLTRB(20, 5, 20, 0),
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text("Ant")
+                    ],),),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
