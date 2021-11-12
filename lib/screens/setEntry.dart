59d148a4bad1051eef4e47cb14ce62e8a0445c7e
Anthony Duong <78009038+AnthonyDuong17@users.noreply.github.com>
2021-11-11 23:18:52 -0600

    setEntry update

--- lib/screens/setEntry.dart
+++ lib/screens/setEntry.dart
@@ -2,136 +2,226 @@
 
 import 'dart:ui';
 
-import 'package:firstapp/widgets/addSet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-class setEntry extends StatefulWidget {
-  const setEntry({Key? key}) : super(key: key);
-
-  @override
-  _setEntry createState() => _setEntry();
-}
-
-class _setEntry extends State<setEntry> {
-  int numberOfWidgets = 1;
-
-  List<Widget> generateWidgets(numberOfWidgets) {
-    final exerciseWidgets = List<Widget>.generate(
-      10,
-      (numberOfWidgets) => Container(
-        height: 150,
-        child: Column(
-          children: [
-            Container(height: 75, child: addSet()),
-            Text("$numberOfWidgets")
-          ],
-        ),
-      ),
-    );
-    return exerciseWidgets;
-  }
-
+class setEntry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('Enter Set Information',
-              style: TextStyle(fontWeight: FontWeight.bold)),
-          backgroundColor: Colors.red),
-      body: NotificationListener<OverscrollIndicatorNotification>(
-        onNotification: (OverscrollIndicatorNotification overScroll) {
-          overScroll.disallowGlow();
-          return false;
-        },
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Iso-Lateral Pulldowns (hardcoded)',
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text('Enter Set Information'), backgroundColor: Colors.red),
+        body: Center(
+          child: NotificationListener<OverscrollIndicatorNotification>(
+            onNotification: (OverscrollIndicatorNotification overScroll) {
+              overScroll.disallowGlow();
+              return false;
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        margin: EdgeInsets.only(
+                          left: 25,
+                          top: 30,
+                          right: 25,
+                        ),
+                        child: Text(
+                          'Iso-Lateral Pulldowns (hardcoded)',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  SizedBox(
+                    height: 75,
+                    width: 500,
+                    child: Card(
+                      elevation: 0,
+                      color: Colors.grey.shade200,
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Padding(
+                        // padding: const EdgeInsets.all(20.0),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 10),
+                        child: Center(
+                          child: ListView(
+                            physics: BouncingScrollPhysics(),
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Sets:',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 45,
+                                    height: 35,
+                                    child: Card(
+                                      elevation: 0,
+                                      color: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                      child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 20),
+                                        maxLines: 1,
+                                        decoration: InputDecoration.collapsed(
+                                            hintText: '0'),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Reps:',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 45,
+                                    height: 35,
+                                    child: Card(
+                                      elevation: 0,
+                                      color: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                      child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 20),
+                                        maxLines: 1,
+                                        decoration: InputDecoration.collapsed(
+                                            hintText: '0'),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    'Weight:',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 45,
+                                    height: 35,
+                                    child: Card(
+                                      elevation: 0,
+                                      color: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                      child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(fontSize: 20),
+                                        maxLines: 1,
+                                        decoration: InputDecoration.collapsed(
+                                            hintText: '0'),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: numberOfWidgets,
-                    key: UniqueKey(),
-                    itemBuilder: (),
-                  )
-                )
-              ],
+                  Row(
+                    // Row to align buttons
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(width: 20, height: 80),
+                      Expanded(
+                        child: SizedBox(
+                          height: 40,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(15),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Add Set',
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          height: 40,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.red,
+                              elevation: 0,
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(15),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Save Set',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20)
+                    ],
+                  ),
+                  SizedBox(
+                    // A buffer to keep the screen at the top
+                    height: 500,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-          decoration: BoxDecoration(color: Colors.grey.shade100),
-          height: 75,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                height: 40,
-                width: 150,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    elevation: 0,
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(15),
-                    ),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      numberOfWidgets = numberOfWidgets + 1;
-                    });
-                  },
-                  child: Text(
-                    'Add Another Set',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 40,
-                width: 150,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    elevation: 0,
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(15),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Save Workout',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )),
     );
   }
 }
